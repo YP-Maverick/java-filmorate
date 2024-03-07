a504528397ca4f3d261921695cb9943e8c18ce21
Maverick <Egor.Ivanov.v@yandex.ru>
2024-03-07 23:30:12 +0300

    Убраны не используемые импорты

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.exception;
 
-//@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NotFoundException extends RuntimeException  {
     public NotFoundException(String message) {
         super(message);
