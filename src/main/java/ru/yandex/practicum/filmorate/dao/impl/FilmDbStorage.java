03fd4160fb233003de8901cc532272cc4525f9d6
PAndrey <PAndrey87@yandex.ru>
2024-05-08 01:07:02 +0300

    fix: hotFix по тестам

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmDbStorage.java
@@ -3,13 +3,11 @@ package ru.yandex.practicum.filmorate.dao.impl;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.dao.FilmStorage;
 import ru.yandex.practicum.filmorate.dao.mapper.ModelMapper;
-import ru.yandex.practicum.filmorate.exception.LikeException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 
@@ -107,17 +105,17 @@ public class FilmDbStorage implements FilmStorage {
     @Override
     public void addLike(Long filmId, Long userId) {
         log.debug("Запрос пользователя с id {} добавить лайк фильму с id {}.", userId, filmId);
+        String checkSql = "SELECT COUNT(*) FROM film_likes WHERE film_id = ? AND user_id = ?";
+        int checkLikes = jdbcTemplate.queryForObject(checkSql, Integer.class, filmId, userId);
 
-        String sql = "INSERT INTO film_likes (film_id, user_id) VALUES (?, ?)";
-        try {
+        if (checkLikes == 0) {
+            String sql = "INSERT INTO film_likes (film_id, user_id) VALUES (?, ?)";
             jdbcTemplate.update(sql, filmId, userId);
-        } catch (DataAccessException e) {
-            log.error("Запрос пользователя ({}) повторно поставить лайк фильму ({}).", userId, filmId);
-            throw new LikeException(String.format("Пользователь %d уже поставил лайк фильму %d.",
-                    userId, filmId));
+            String filmLikeSql = "UPDATE films SET likes = likes + 1 WHERE id = ?";
+            jdbcTemplate.update(filmLikeSql, filmId);
+        } else {
+            log.info("Пользователь с id {} уже поставил лайк фильму с id {}.", userId, filmId);
         }
-        String filmLikeSql = "UPDATE films SET likes = likes + 1 WHERE id = ?";
-        jdbcTemplate.update(filmLikeSql, filmId);
     }
 
     @Override
@@ -136,7 +134,7 @@ public class FilmDbStorage implements FilmStorage {
     public List<Film> getTopFilms(Integer count, Integer genreId, String year) {
         log.debug("Получен запрос вывести список популярных фильмов");
 
-        String baseSql = "SELECT f.*, "
+        String baseSql = "SELECT DISTINCT f.*, "
                 + "rm.name AS rating_name "
                 + "FROM films f "
                 + "JOIN rating_MPA rm ON rm.ID = f.rating_id "
